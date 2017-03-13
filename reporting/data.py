48c6c4cc50f758794bbf5462cbee6cf9afe75f9a
Maxim Filippov <icorso@yandex.ru>
2017-03-13 15:46:52 +0300

    view cleanup

--- reporting/data.py
+++ reporting/data.py
@@ -3,280 +3,111 @@ import datetime
 from constants import EventType as Et
 from constants import SettlementStatus as Ss
 from constants import TransactionStatus as Ts
-from reporting.response import WSEventReport, ACHJHResponse
+from reporting.reports.historical_event_report import WSEventReport
 
 now = (datetime.datetime.now()).replace(hour=0, minute=0, second=0)
 
-VOIDED = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.VOIDED.status,
-        SettlementStatus=Ss.NO_SETTLEMENT_NEEDED.status,
-        EventDateTime=now - datetime.timedelta(minutes=5))
-    ).with_event(WSEventReport(
-        EventType=Et.VOIDED.status,
-        TransactionStatus=Ts.VOIDED.status,
-        SettlementStatus=Ss.NO_SETTLEMENT_NEEDED.status,
-        EventDateTime=now - datetime.timedelta(minutes=3))
-    )
 
-SETTLED = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=22))
-    ).with_event(WSEventReport(
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=20))
-    ).with_event(WSEventReport(
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=2))
-    ).with_event(WSEventReport(
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=3, hours=7))
-    # ).with_event(WSEventReport(
-    #     EventType=Et.RETURNED_NSF.status,
-    #     TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-    #     SettlementStatus=Ss.CHARGED_BACK.status,
-    #     EventDateTime=now - datetime.timedelta(days=2, hours=1, minutes=40))
-    # ).with_event(WSEventReport(
-    #     EventType=Et.RETURNED_NSF.status,
-    #     TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-    #     SettlementStatus=Ss.CHARGED_BACK.status,
-    #     EventDateTime=now - datetime.timedelta(days=1))
-    # ).with_event(WSEventReport(
-    #     EventType=Et.RETURNED_NSF.status,
-    #     TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-    #     SettlementStatus=Ss.CHARGED_BACK.status,
-    #     EventDateTime=now - datetime.timedelta(hours=12))
-    # remove
-    # ).with_event(WSEventReport(
-    #     EventType=Et.APPROVED.status,
-    #     TransactionStatus=Ts.APPROVED.status,
-    #     SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-    #     EventDateTime=now - datetime.timedelta(days=0, hours=2))
-    # ).with_event(WSEventReport(
-    #     EventType=Et.RETURNED_NSF.status,
-    #     TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-    #     SettlementStatus=Ss.CHARGED_BACK.status,
-    #     EventDateTime=now - datetime.timedelta(hours=1))
-    )
+def approved(tn, rn, at=now - datetime.timedelta(days=9, hours=22)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.APPROVED.status, EventDateTime=at,
+                         TransactionStatus=Ts.APPROVED.status, SettlementStatus=Ss.TO_BE_ORIGINATED.status)
 
-BAD_ACCOUNT = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=6, hours=22))
-    ).with_event(WSEventReport(  # Processed
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=6, hours=20))
-    ).with_event(WSEventReport(  # Originated
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=6, hours=4))
-    ).with_event(WSEventReport(  # Settled
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=4))
-    ).with_event(WSEventReport(
-        EventType=Et.BAD_ACCOUNT.status,
-        TransactionStatus=Ts.CLOSED_ACCOUNT.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=3, hours=14)
-    ))
 
-APPROVED = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=3, hours=6))
-    ).with_event(WSEventReport(
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=22))
-    ).with_event(WSEventReport(
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1, hours=16))
-    ).with_event(WSEventReport(
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=1, hours=15, minutes=59))
-    )
+def processed(tn, rn, at=now - datetime.timedelta(days=9, hours=20)):
+    return WSEventReport(EventType=Et.PROCESSED.status, TransactionNumber=tn, ReferenceNumber=rn, EventDateTime=at,
+                         TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.TO_BE_ORIGINATED.status)
 
-COLLECTED = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=22))
-    ).with_event(WSEventReport(  # Processed
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=20))
-    ).with_event(WSEventReport(
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=9))
-    ).with_event(WSEventReport(
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=8))
-    ).with_event(WSEventReport(
-        EventType=Et.RETURNED_NSF.status,
-        TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=8, hours=10))
-    ).with_event(WSEventReport(
-        EventType=Et.SENT_TO_COLLECTION.status,
-        TransactionStatus=Ts.IN_COLLECTION.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=4, hours=22))
-    ).with_event(WSEventReport(
-        EventType=Et.COLLECTION_FAILED.status,
-        TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=3, hours=22))
-    ).with_event(WSEventReport(
-        EventType=Et.COLLECTED.status,
-        TransactionStatus=Ts.COLLECTED.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=22))
-    )
 
-RETURNED_NSF = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=22))
-    ).with_event(WSEventReport(
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=20))
-    ).with_event(WSEventReport(
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=9))
-    ).with_event(WSEventReport(
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=8))
-    ).with_event(WSEventReport(
-        EventType=Et.RETURNED_NSF.status,
-        TransactionStatus=Ts.UNCOLLECTED_NSF.status,
-        SettlementStatus=Ss.CHARGED_BACK.status,
-        EventDateTime=now - datetime.timedelta(days=5, hours=4)))
+def originated(tn, rn, at=now - datetime.timedelta(days=8)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.ORIGINATED.status,
+                         TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.PENDING.status, EventDateTime=at)
 
-REFUNDED = ACHJHResponse().with_event(WSEventReport(  # Approved
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=22))
-    ).with_event(WSEventReport(  # Processed
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=20))
-    ).with_event(WSEventReport(  # Originated
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=9))
-    ).with_event(WSEventReport(  # Settled
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=8))
-    ).with_event(WSEventReport(
-        EventType=Et.REFUNDED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=5)))
 
-REFUND = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=5))
-    ).with_event(WSEventReport(
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1, hours=4))
-    ).with_event(WSEventReport(
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1))
-    ).with_event(WSEventReport(
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1)))
+def settled(tn, rn, at=now - datetime.timedelta(days=8)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.SETTLED.status,
+                         TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.SETTLED.status, EventDateTime=at)
 
-FULL_REFUND = ACHJHResponse().with_event(WSEventReport(
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.APPROVED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=22))
-    ).with_event(WSEventReport(  # Processed
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.TO_BE_ORIGINATED.status,
-        EventDateTime=now - datetime.timedelta(days=9, hours=20))
-    ).with_event(WSEventReport(  # Originated
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=9))
-    ).with_event(WSEventReport(  # Settled
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=8))
-    ).with_event(WSEventReport(
-        EventType=Et.REFUNDED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.SETTLED.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=5))
-    ).with_event(WSEventReport(
-        ReferenceNumber='PHSTSKYNUGQ',
-        EventType=Et.APPROVED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=2, hours=5))
-    ).with_event(WSEventReport(
-        ReferenceNumber='PHSTSKYNUGQ',
-        EventType=Et.PROCESSED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1, hours=4))
-    ).with_event(WSEventReport(
-        ReferenceNumber='PHSTSKYNUGQ',
-        EventType=Et.ORIGINATED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1))
-    ).with_event(WSEventReport(
-        ReferenceNumber='PHSTSKYNUGQ',
-        EventType=Et.SETTLED.status,
-        TransactionStatus=Ts.PROCESSED.status,
-        SettlementStatus=Ss.PENDING.status,
-        EventDateTime=now - datetime.timedelta(days=1)))
+
+def returned_nsf(tn, rn, at=now - datetime.timedelta(days=3)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.RETURNED_NSF.status, EventDateTime=at,
+                         TransactionStatus=Ts.UNCOLLECTED_NSF.status, SettlementStatus=Ss.CHARGED_BACK.status)
+
+
+def sent_to_collection(tn, rn, at=now - datetime.timedelta(days=2)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventDateTime=at,
+                         EventType=Et.SENT_TO_COLLECTION.status, TransactionStatus=Ts.IN_COLLECTION.status,
+                         SettlementStatus=Ss.CHARGED_BACK.status)
+
+
+def collection_failed(tn, rn, at=now):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventDateTime=at,
+                         EventType=Et.COLLECTION_FAILED.status, TransactionStatus=Ts.UNCOLLECTED_NSF.status,
+                         SettlementStatus=Ss.CHARGED_BACK.status)
+
+
+def returned_bad_account(tn, rn, at=now - datetime.timedelta(days=3)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventDateTime=at,
+                         EventType=Et.RETURNED_BAD_ACCOUNT.status, TransactionStatus=Ts.CLOSED_ACCOUNT.status,
+                         SettlementStatus=Ss.CHARGED_BACK.status)
+
+
+def refunded(tn, rn, at=now - datetime.timedelta(days=2, hours=5)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.REFUNDED.status,
+                         TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.SETTLED.status, EventDateTime=at)
+
+
+def r01(tn, rn, at=now - datetime.timedelta(days=1)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.BAD_ACCOUNT.status, EventDateTime=at,
+                         TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.NO_SETTLEMENT_NEEDED.status,
+                         ReturnCode='R01')
+
+
+def r02(tn, rn, at=now - datetime.timedelta(days=1)):
+    return WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.BAD_ACCOUNT.status, EventDateTime=at,
+                         TransactionStatus=Ts.CLOSED_ACCOUNT.status, SettlementStatus=Ss.NO_SETTLEMENT_NEEDED.status,
+                         ReturnCode='R02')
+
+
+def refund_set(tn, rn):
+    return [WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.APPROVED.status,
+                          TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.PENDING.status,
+                          EventDateTime=now - datetime.timedelta(days=2, hours=5)),
+            WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.PROCESSED.status,
+                          TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.PENDING.status,
+                          EventDateTime=now - datetime.timedelta(days=1, hours=4)),
+            WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.ORIGINATED.status,
+                          TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.PENDING.status,
+                          EventDateTime=now - datetime.timedelta(days=1)),
+            WSEventReport(TransactionNumber=tn, ReferenceNumber=rn, EventType=Et.SETTLED.status,
+                          TransactionStatus=Ts.PROCESSED.status, SettlementStatus=Ss.PENDING.status,
+                          EventDateTime=now - datetime.timedelta(days=1))]
+
+
+def settled_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn), settled(tn, rn)]
+
+
+def processed_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn)]
+
+
+def originated_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn)]
+
+
+def returned_nsf_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn), settled(tn, rn), returned_nsf(tn, rn)]
+
+
+def returned_bad_account_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn), settled(tn, rn), returned_bad_account(tn, rn)]
+
+
+def sent_to_collection_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn), settled(tn, rn), returned_nsf(tn, rn),
+            sent_to_collection(tn, rn)]
+
+
+def collection_failed_set(tn, rn):
+    return [approved(tn, rn), processed(tn, rn), originated(tn, rn), settled(tn, rn), sent_to_collection(tn, rn),
+            collection_failed(tn, rn, at=now - datetime.timedelta(days=1))]
